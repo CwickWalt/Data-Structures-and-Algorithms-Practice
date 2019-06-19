ab72070719bfae327813e7c30c75fb1ca782db88
CwickWalt <chadwalt21@gmail.com>
2019-06-19 17:44:12 -0400

    update mergeSortedLists.py

--- mergeSortedLists.py
+++ mergeSortedLists.py
@@ -1,23 +1,33 @@
 def mergeSortedLists(list1, list2, mergedList):
     list1Item = list1[0]
     list2Item = list2[0]
-
+    # if EITHER list1 OR list2 are NOT empty, continue.
     if len(list1) > 0 or len(list2) > 0:
+        # if list1 is empty, add list2 onto mergedList and return mergedList.
         if len(list1) == 0:
             mergedList.extend(list2)
             return mergedList
+        # if list2 is empty, add list1 onto mergedList and return mergedList.
         elif len(list2) == 0:
             mergedList.extend(list1)
             print(mergedList)
+        # if both lists are NOT empty, find the smallest number of each list 
+        # at index 0, pop it, then append to mergedList
         else:
+            # if list1 contains the smaller number at index 0, pop it from the 
+            # list, then append it to mergedList
             if list1Item <= list2Item:
                 list1Item = list1.pop(0)
                 mergedList.append(list1Item)
+                # if the previous operation leaves list1 empty, add list2 to 
+                # mergedList and print mergedList
                 if len(list1) == 0:
                     mergedList.extend(list2)
                     print(mergedList)
                 else:
+                    # if list1 is NOT empty, call the function again
                     mergeSortedLists(list1, list2, mergedList)
+            # same as above if statement, but repeated for list2
             elif list1Item >= list2Item:
                 list2Item = list2.pop(0)
                 mergedList.append(list2Item)
@@ -26,6 +36,7 @@ def mergeSortedLists(list1, list2, mergedList):
                     print(mergedList)
                 else:
                     mergeSortedLists(list1, list2, mergedList)
+    # if line 5 of the function is false, print mergedList
     else:
         print(mergedList)
 
